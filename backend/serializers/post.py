34ba547bb7e5b71871ff245c9ee4e428d5a348aa
D1VORY <dubchakob@gmail.com>
2020-09-27 16:27:08 +0300

    added auth

--- backend/serializers/post.py
+++ backend/serializers/post.py
@@ -2,11 +2,12 @@ from rest_framework import serializers
 from backend.models import Post
 from django.contrib.auth.models import User
 from rest_framework.serializers import raise_errors_on_nested_writes
+from django.contrib.auth.models import AnonymousUser
 import copy
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = serializers.CharField(source="author.username")
+    author = serializers.CharField(source="author.username", read_only=True)
     is_upvoted_by_current_user = serializers.SerializerMethodField(
         "_is_upvoted_by_current_user"
     )
@@ -27,6 +28,7 @@ class PostSerializer(serializers.ModelSerializer):
             "is_upvoted_by_current_user",
             "created",
             "id",
+            "author",
         ]
 
     def validate_author(self, value):
@@ -42,38 +44,25 @@ class PostSerializer(serializers.ModelSerializer):
         checks if the post was upvoted by
         current logged in user
         """
-
-        request = self.context.get("request", None)
-        if request and request.user.is_authenticated:
-            return obj.upvotes.filter(pk=request.user.id).exists()
+        user = self._get_current_user()
+        if not user.is_anonymous:
+            return obj.upvotes.filter(pk=user.id).exists()
 
         return False
 
     def create(self, validated_data):
+        """
+        creates new post
+        """
+        # due to permission_classes, user has to be authenticated
         return Post.objects.create(
             title=validated_data["title"],
             link=validated_data["link"],
-            author=User.objects.get(username=validated_data["author"]["username"]),
+            author=self._get_current_user(),
         )
 
-    def update(self, instance, validated_data):
-        """
-        performs update of instance
-        """
-
-        upd_dict = copy.deepcopy(validated_data)  # to not modify validated data
-        try:
-            # to update nested fields
-            upd_dict["author"] = User.objects.get(
-                username=upd_dict["author"]["username"]
-            )
-        except KeyError:
-            upd_dict.pop("author", None)
-
-        for attr, value in upd_dict.items():
-            setattr(instance, attr, value)
-        instance.save()
-
-        # p.s. I know this is a bad way to do this, but didn't find a
-        # proper solution for updating nested fields
-        return instance
+    def _get_current_user(self):
+        request = self.context.get("request", None)
+        if request and request.user.is_authenticated:
+            return request.user
+        return AnonymousUser()
--- dashboard/settings.py
+++ dashboard/settings.py
@@ -45,8 +45,15 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "django_celery_beat",
+    "rest_framework.authtoken",
     "backend",
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+    ]
+}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
