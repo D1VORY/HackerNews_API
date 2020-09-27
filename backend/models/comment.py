b945fda179d01f519d64d299ecdaa90ecd25fa7a
D1VORY <dubchakob@gmail.com>
2020-09-27 03:46:08 +0300

    created crud comments api

--- backend/models/comment.py
+++ backend/models/comment.py
@@ -4,8 +4,9 @@ from django.contrib.auth.models import User
 
 class Comment(models.Model):
     content = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
     created = models.DateTimeField(auto_now_add=True, verbose_name="date of creation")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    post = models.ForeignKey("Post", on_delete=models.CASCADE, related_name="comments")
 
     def __str__(self):
         return f"{self.content}"[:40]
